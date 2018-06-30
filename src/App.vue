4a4b29340b3d7badc6c930342f7ec956fa5e6e10
Your Name <1218866243@qq.com>
2018-06-30 15:45:52 +0800

    配置组件

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <!-- 放头部组件 -->
+    <app-header></app-header>
+    <div class="container-fluid">
+      <div class="row">
+        <!-- 放侧边栏 -->
+          <app-sidebar></app-sidebar>
+          <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+            <router-view></router-view>
+          </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import appHeader from '@/components/app-header'
+import appSidebar from '@/components/app-sidebar'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    appHeader,
+    appSidebar
+  }
 }
 </script>
 
